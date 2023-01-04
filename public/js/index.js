6dc5b1b7e57c7c9a5ba5b2a614439a25c7a0d1d9
sebastian Reyes <sebreyes222gmail.com>
2023-01-04 10:03:37 +0100

    ajout des log

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,14 @@
 let francois = {
     panier : ['tomate', 'lait', 'patate', 'carotte', 'pain'],
-    
+    derober(sergio){
+        francois.panier.push(sergio.panier.pop(), sergio.panier.pop())
+    }
 }
 let sergio = {
     panier : ['courgette', 'glace', 'thon','jus', 'coca', ],
 }
+console.log(francois.panier);
+console.log(sergio.panier);
+francois.derober(sergio)
+console.log(francois.panier);
+console.log(sergio.panier);
